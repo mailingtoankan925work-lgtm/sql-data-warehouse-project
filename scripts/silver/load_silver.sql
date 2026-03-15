62d7eceaf4a3ef71c0a3b2ae017eb54dc4665234
mailingtoankan925work-lgtm <mailingtoankan925work@gmail.com>
2026-03-15 19:51:04 +0530

    Update load_silver.sql

--- scripts/silver/load_silver.sql
+++ scripts/silver/load_silver.sql
@@ -13,7 +13,7 @@ Parameters:
     This stored procedure does not accept any parameters or return any values.
 
 Usage Example:
-    EXEC bronze.load_silver;
+    EXEC silver.load_silver;
 =========================================================================================================
 */
 CREATE OR ALTER PROCEDURE silver.load_silver AS
